8ae28e7a04f6efbd7d58e2dfdd16084a69decc91
Fifinella <h.hoesley@gmail.com>
2023-03-24 12:33:19 -0700

    added contact page

---
+++ src/pages/ContactPage/index.js
@@ -0,0 +1,19 @@
+const ContactPage = () => {
+  return (
+    <form>
+      <div>
+        <label>Name</label>
+        <input />
+      </div>
+      <div>
+        <label>Email</label>
+        <input />
+      </div>
+      <div>
+        <label>Message</label>
+        <input />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
