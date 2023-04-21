a917a72453b5b9243cc514ba9ff4a1cbe781bff9
Fifinella <h.hoesley@gmail.com>
2023-04-21 14:08:42 -0700

    added Resume

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import PortfolioPage from "./pages/PortfolioPage";
 import ContactPage from "./pages/ContactPage";
 import AboutMe from "./pages/AboutMePage";
+import ResumePage from "./pages/ResumePage";
 import { useState } from "react";
 import Footer from "./components/footer/index.js";
 
@@ -34,11 +35,20 @@ function App() {
           >
             Contact
           </li>
+          <li
+            className={currentPage === "Resume" && "active"}
+            onClick={() => {
+              setCurrentPage("Resume");
+            }}
+          >
+            Resume
+          </li>
         </ul>
       </nav>
       {currentPage === "Contact" && <ContactPage />}
       {currentPage === "Portfolio" && <PortfolioPage />}
       {currentPage === "About Me" && <AboutMe />}
+      {currentPage === "Resume" && <ResumePage />}
       <Footer />
     </div>
   );
---
+++ src/pages/ResumePage/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ResumePage = () => {
+  return (
+    <div className="container">
+      <p>
+        <a href="Helen_Evans_Resume.pdf" download="Helen_Evans_Resume.pdf">
+          Download Resume
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default ResumePage;
